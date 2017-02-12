b2db14c80885af45369a5a5d0e31b1736bd2a28c
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-12 13:20:43 +0530

    Initial commit

---
+++ src/main/java/my/spike/FlywayJpaH2SpikeApplication.java
@@ -0,0 +1,12 @@
+package my.spike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayJpaH2SpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayJpaH2SpikeApplication.class, args);
+	}
+}
